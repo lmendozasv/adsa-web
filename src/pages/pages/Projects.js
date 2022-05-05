c0599133915f47b99fb115a23202e91327cc6e1e
Luis Ernesto Mendoza Sánchez <lmendoza.sv@gmail.com>
2022-05-04 18:52:26 -0600

    CTASK0053818-Updating-DF-SDK-To-2.0.0.1:a0c0d3

--- src/pages/pages/Projects.js
+++ src/pages/pages/Projects.js
@@ -416,8 +416,7 @@ function EnhancedTable({ dataRows, ins }) {
                       <TableCell align="left">{row.dt_pedido}</TableCell>
                       <TableCell align="left">{row.dt_envio}</TableCell>
                       <TableCell align="left">{row.nombre_cliente}</TableCell>
-                      <TableCell>  
-                                              
+                      <TableCell>                                                
                             <Chip
                               size="small"
                               mr={1}
